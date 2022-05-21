aefff26dc1a724673ead38f0e7dec875cc30ddd1
Sascvotch <sascvotch@yandex.ru>
2022-05-21 14:19:11 +0300

    2.16

--- src/test/java/SortTest.java
+++ src/test/java/SortTest.java
@@ -3,9 +3,11 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SortTest {
     private final SortArr sortArr = new SortArr();
-    private MyIntList arr = new MyIntList(0);
+    private MyIntList arr = new MyIntList(5);
     private MyIntList tempArrInsertion = new MyIntList(0);
     private MyIntList tempArrSelection = new MyIntList(0);
     private MyIntList tempArrBubble = new MyIntList(0);
@@ -21,7 +23,7 @@ public class SortTest {
 
     @Test
     public void speed() throws MyIntList.MyException {
-        arr = arr.generateRandomArray(10000);
+        arr = arr.generateRandomArray(1000);
         tempArrInsertion = tempArrInsertion.copy(arr);
         tempArrBubble = tempArrBubble.copy(arr);
         tempArrSelection = tempArrSelection.copy(arr);
@@ -62,21 +64,36 @@ public class SortTest {
     }
 
     @Test
-    public void containsTest () throws MyIntList.MyException {
+    public void containsTest() throws MyIntList.MyException {
         arr.add(0);
         arr.add(4);
         arr.add(2);
         arr.add(1);
         arr.add(1);
         arr.add(5);
-        Integer item=2;
+        Integer item = 2;
         if (item == null) {
             throw new MyIntList.MyException("Введите значение");
         } else {
-            sortArr.sortInsertion(arr);
+            //sortArr.sortInsertion(arr);
+            sortArr.quickSort(arr,0,arr.size()-1);
             arr.binarySearch(arr, item);
             System.out.println(arr.toString());
         }
+    }
+
+    @Test
+    public void addTest() throws MyIntList.MyException {
+        arr.add(8);
+        arr.add(2);
+        arr.add(1);
+        arr.add(1);
+        arr.add(5);
+        arr.add(6);
+        arr.addIndexItem(2, 0);
+        arr.add(9);
+        System.out.println(arr.toString());
+        assertEquals(10, arr.size());
     }
 
 }
